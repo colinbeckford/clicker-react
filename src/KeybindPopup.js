350ab6691767c9dd204341e16c9352784654a3e5
Colin Beckford <colinsbeckford@gmail.com>
2024-08-08 12:09:58 -0400

    removing use effect

--- src/KeybindPopup.js
+++ src/KeybindPopup.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 function KeybindPopup({ keybinds, setKeybinds, handlePopupClose }) {
   const [newKeybinds, setNewKeybinds] = useState(keybinds);
